dabfc8e4fc30858cab6806abf30515dc08bf3bdc
wahid_md <amwahid2004@gmail.com>
2025-09-13 09:23:40 +0530

    #

--- includes/config.php
+++ includes/config.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "membershiphp";
+$username = "u763811849_root9";
+$password = "Anees@9999";
+$dbname = "u763811849_membershiphp";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
